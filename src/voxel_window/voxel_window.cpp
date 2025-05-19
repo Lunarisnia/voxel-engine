61ce54615c928de7bec7a8f140cfc8f573367da1
Lounarisnia <r.arswendo.r@gmail.com>
2025-05-19 17:46:58 +0700

    wip: voxel engine

--- src/voxel_window/voxel_window.cpp
+++ src/voxel_window/voxel_window.cpp
@@ -45,25 +45,83 @@ VoxelWindow::VoxelWindow(int width, int height, const char *title)
   glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
 }
 
-void renderDummyTriangle() {
-  GLfloat vertices[] = {
-      0.5f,  0.0f,  0.0f,  //
-      0.5f,  -0.5f, 0.0f,  //
-      -0.5f, -0.5f, 0.0f,
-  };
+void VoxelWindow::show() {
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  ;
 
-  GLuint VBO;
+  const char *vertexShaderSource =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main()\n"
+      "{\n"
+      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+      "}\0";
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  GLint code;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &code);
+  if (!code) {
+    char infoLog[512];
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+
+    std::cout << "VERTEX: " << infoLog << std::endl;
+  }
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  const char *fragmentShaderSource =
+      "#version 330 core\n"
+      "out vec4 FragColor;\n"
+      "void main() {\n"
+      " FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+      "}\n\0";
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &code);
+  if (!code) {
+    char infoLog[512];
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "FRAGMENT: " << infoLog << std::endl;
+  }
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &code);
+  if (!code) {
+    char infoLog[512];
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "PROGRAM: " << infoLog << std::endl;
+  }
+
+  glDeleteShader(fragmentShader);
+  glDeleteShader(vertexShader);
+
+  float vertices[] = {0.0f,  0.5f,  0.0f,  //
+                      0.5f,  -0.5f, 0.0f,  //
+                      -0.5f, -0.5f, 0.0f};
+
+  GLuint VBO, VAO;
+  glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-};
 
-void VoxelWindow::show() {
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    renderDummyTriangle();
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
