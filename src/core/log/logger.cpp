d045ba2d4a264e48db4fe49f77ee56c05303e3e3
Lounarisnia <r.arswendo.r@gmail.com>
2025-05-23 16:22:18 +0700

    feat: added log symbol

--- src/core/log/logger.cpp
+++ src/core/log/logger.cpp
@@ -1,8 +1,22 @@
 #include "voxelengine/log/logger.hpp"
 #include <iostream>
+#include <string>
 using namespace VoxelEngine;
 void Logger::Log(LogCategory logCategory, const std::string &message,
                  const std::string &context) {
+  std::string logSymbol;
+  if (logCategory == LogCategory::INFO) {
+    logSymbol = "[INFO]";
+  } else if (logCategory == LogCategory::ERROR) {
+    logSymbol = "[ERROR]";
+  } else if (logCategory == LogCategory::DEBUG) {
+    logSymbol = "[DEBUG]";
+  } else {
+    logSymbol = "[UNKNOWN]";
+  }
+
   // TODO: Add timestamp
-  std::cout << "[" << context << "]: " << message << std::endl;
+  std::cout << logSymbol << "[" << context << "]: " << message << std::endl;
 }
+
+// TODO: add system to allow logging into files
